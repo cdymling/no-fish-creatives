b7a514c962f172cb00459535eedd5d643a4c484d
cdymling <christoffer.dymling@gmail.com>
2025-02-02 12:30:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,19 @@ import Contact from "./pages/Contact";
 const App = () => (
   <Layout>
     <div className="snap-y snap-mandatory h-screen overflow-y-scroll">
-      <section id="home" className="snap-start h-screen w-full">
+      <section id="home" className="snap-start h-screen overflow-y-scroll">
         <Home />
       </section>
-      <section id="intro" className="snap-start h-screen w-full">
+      <section id="intro" className="snap-start h-screen overflow-y-scroll">
         <Intro />
       </section>
-      <section id="about" className="snap-start h-screen w-full">
+      <section id="about" className="snap-start h-screen overflow-y-scroll">
         <About />
       </section>
-      <section id="work" className="snap-start h-screen w-full">
+      <section id="work" className="snap-start h-screen overflow-y-scroll">
         <Work />
       </section>
-      <section id="contact" className="snap-start h-screen w-full">
+      <section id="contact" className="snap-start h-screen overflow-y-scroll">
         <Contact />
       </section>
     </div>
